6802acccf1316427dd078def34633fb1c4355e51
shindori fumiya <shindori238@gmail.com>
2023-11-25 22:00:46 +0900

    Add Skill Icon

--- pages/profile.vue
+++ pages/profile.vue
@@ -5,9 +5,9 @@
 
   <div class="px-20 flex justify-evenly">
     <div class="w-1/3">
-      <p class="text-3xl">WHO AM I</p>
+      <p class="text-3xl text-center mb-4">WHO AM I</p>
       <UCard>
-        <UDivider>
+        <UDivider class="mb-4">
           <ULink to="https://github.com/fumiyanokesinn" target="_blank">
             <img
               class="rounded-full"
@@ -16,12 +16,35 @@
             />
           </ULink>
         </UDivider>
-        aaaaaaa
+        <div>
+          <ui>
+            <li class="flex items-center space-x-4 mb-2">
+              <UBadge size="lg" class="w-20">Job</UBadge>
+              <p class="text-xl">Web Programmer</p>
+            </li>
+            <li class="flex items-center space-x-4 mb-2">
+              <UBadge size="lg" class="w-20">Name</UBadge>
+              <p class="text-xl">Shindori Fumiya</p>
+            </li>
+            <li class="flex items-center space-x-4 mb-2">
+              <UBadge size="lg" class="w-20">Hobby</UBadge>
+              <p class="text-xl">Game Creator</p>
+            </li>
+          </ui>
+        </div>
       </UCard>
     </div>
     <div class="w-1/3">
-      <p class="text-3xl">My Skill</p>
-      <UCard> ssssss</UCard>
+      <p class="text-3xl text-center mb-4">My Skill</p>
+      <UCard>
+        <p>
+          <NuxtLink to="https://skillicons.dev" target="_blank">
+            <img
+              src="https://skillicons.dev/icons?i=git,docker,java,php,laravel,js,react,redux,vue,nuxtjs"
+            />
+          </NuxtLink>
+        </p>
+      </UCard>
     </div>
   </div>
 </template>
