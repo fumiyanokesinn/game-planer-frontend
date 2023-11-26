2da75c1e7f38bf2128a62160ced8251f80b3c8aa
shindori fumiya <shindori238@gmail.com>
2023-11-26 17:38:30 +0900

    Add IconButton

---
+++ components/IconButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  const props = defineProps({
+    iconName: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: String,
+      default: '1em'
+    },
+    onClick: {
+      type: Function,
+      default(event: MouseEvent) {
+        console.log(event);
+      }
+    }
+  });
+
+  function onclick(event: MouseEvent) {
+    props.onClick(event);
+  }
+</script>
+
+<template>
+  <button v-on:click="onclick">
+    <Icon :name="iconName" :size="size" />
+  </button>
+</template>
