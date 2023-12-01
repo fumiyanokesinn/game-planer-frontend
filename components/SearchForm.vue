190db3bc1c39002d5319e197130f1df4c2e142c8
shindori fumiya <shindori238@gmail.com>
2023-12-02 01:29:45 +0900

    FormにSelectを追加

--- components/SearchForm.vue
+++ components/SearchForm.vue
@@ -1,13 +1,29 @@
 <script setup>
+  const countries = ['United States', 'Canada', 'Mexico'];
   const value = ref('');
 </script>
 
 <template>
-  <UInput
-    icon="i-heroicons-magnifying-glass-20-solid"
-    v-model="value"
-    placeholder="Search..."
-    color="gray"
-    size="xl"
-  />
+  <UContainer class="w-1/3">
+    <UInput
+      icon="i-heroicons-magnifying-glass-20-solid"
+      v-model="value"
+      placeholder="Search... Please Input Summoner Name"
+      color="gray"
+      size="xl"
+      :ui="{
+        icon: { trailing: { pointer: '' } },
+        trailing: { padding: { xl: 'pe-40' } }
+      }"
+    >
+      <template #trailing>
+        <USelect
+          class="z-10"
+          color="yellow"
+          v-model="country"
+          :options="countries"
+        />
+      </template>
+    </UInput>
+  </UContainer>
 </template>
